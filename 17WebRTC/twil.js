ff0e49c02e56210a4be6560e15396486885ecfaa
aman-singh122 <ranjannikhil334@gmail.com>
2026-01-27 17:42:33 +0530

    Initial commit

---
+++ 17WebRTC/twil.js
@@ -0,0 +1,12 @@
+
+// const twilio = require('twilio');
+// // Replace with your actual credentials from Twilio Console
+// const accountSid = '';
+// const authToken = '';
+// const client = twilio(accountSid, authToken);
+// // Generate TURN credentials
+// as
+//     console.error('Error:', error.message);
+//   }
+// }
+// getTurnCredentials();
